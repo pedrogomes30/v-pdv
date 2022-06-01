70f491c0849f5325cc24dc51d6663400793800a4
Pedro <pgs_cf@hotmail.com>
2022-06-01 17:50:59 -0300

    ajuste nas api's

--- pos-back/routes/api.php
+++ pos-back/routes/api.php
@@ -2,20 +2,31 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\v1\AuthController;
+use App\Http\Controllers\API\v1\StoreController;
+use App\Http\Middleware\API\v1\ProtectedRouteAuth;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::prefix('v1')->group(function(){
+    Route::post('login',                [AuthController::class,'login']);
+    Route::middleware([ProtectedRouteAuth::class])->group(function(){
+        //auth
+        Route::post('/logout',           [AuthController::class,'logout']);
+        Route::post('/refresh',          [AuthController::class,'refresh']);
+        Route::post('/me',               [AuthController::class,'me']);
+        //store
+        Route::get('/stores',           [StoreController::class,'index']);
+        Route::get('/stores/id',        [StoreController::class,'show']);
+        Route::post('/store',           [StoreController::class,'store']);
+        Route::post('/store/id',        [StoreController::class,'update']);
+        Route::post('/delete',          [StoreController::class,'delete']);
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
-Route::get('/storegroups',[\App\Http\Controllers\StoreGroupController::class,'index']);
+        
+        //example
+        // Route::get('/tracks',         [TrackController::class,'index']);
+        // Route::get('/tracks/{id}',    [TrackController::class,'show']);
+        // Route::post('/tracks',        [TrackController::class,'store']);
+        // Route::post('/tracks/{id}',   [TrackController::class,'update']);
+        // Route::delete('/tracks/{id}', [TrackController::class,'delete']);
+        
+    });
+});
