2531b1557d886c1deda4ae1635e8fed2d60a3078
Pedro Gomes <pgs_cf@hotmail.com>
2022-10-05 17:53:00 -0300

    Delete config.js
    delete config

--- src/services/api/config.js
+++
@@ -1,6 +0,0 @@
-export default {
-    configs:{
-        BASE_URL    :"http://localhost/pos_back",
-        BASIC_TOKEN : "Basic 6048eb749c6000030fa67ddb8d68ac9006be1ef2fbec4a4c9ef8e9a6ea51"
-    }
-}
